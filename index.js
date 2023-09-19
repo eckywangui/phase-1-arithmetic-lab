f020a50648c0c9e1b9e583b46b93a0da008f1477
Eckra Wangui <+>
2023-09-19 13:38:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
 // Write your code here
+const num1= 1
+const num2= 62
+const multiply=(num1, num2)
+console.log(num1*num2);
+
+
+
+const random= Math.floor(Math.random() * (10-1)) + 1; 
+console.log(random);
+
+
+const num3=15;
+const num4=11;
+
+const mod= (num3 % num4);
+if (mod === 4) {
+    console.log("remainder is 4");
+  } else {
+    console.log("remainder is not 4");
+  }
+
+
+  const numbers= [2,13,7,20,9,17]
+  const max= Math.max(... numbers);
+  console.log(max);
